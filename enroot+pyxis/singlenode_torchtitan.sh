243d830a04197c109206e3a174939358b1613316
roG0d <rodgarcas98@gmail.com>
2025-05-14 18:04:51 +0000

    Naive multinode torchtitan over slurm (Without IB flags)

--- enroot+pyxis/singlenode_torchtitan.sh
+++ enroot+pyxis/singlenode_torchtitan.sh
@@ -4,7 +4,7 @@
 #SBATCH --gpus-per-node=8
 #SBATCH --cpus-per-task=50
 #SBATCH --partition=gpus
-#SBATCH --job-name=torchtitan
+#SBATCH --job-name=torchtitan_singlenode
 #SBATCH -o /home/ubuntu/slurm_logging/headnode/%x_%j_headnode.out
 #SBATCH -e /home/ubuntu/slurm_logging/headnode/%x_%j_headnode.err
 #SBATCH --container-image=/home/ubuntu/torchtitan_cuda128_torch27.sqsh
