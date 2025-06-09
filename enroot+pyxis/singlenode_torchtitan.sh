f5b8f34a6423c63cec7e680b9fa0c204f455be50
roG0d <rodgarcas98@gmail.com>
2025-06-09 08:13:31 +0000

    Added local torchtitan over slurm and script folder

--- enroot+pyxis/singlenode_torchtitan.sh
+++ enroot+pyxis/singlenode_torchtitan.sh
@@ -8,16 +8,6 @@
 #SBATCH -o /home/ubuntu/slurm_logging/headnode/%x_%j.out
 #SBATCH -e /home/ubuntu/slurm_logging/headnode/%x_%j.err
 
-export ENROOT_RUNTIME_PATH=/mnt/local_disk/enroot/runtime/${UID}/${SLURM_JOB_ID}
-export ENROOT_CONFIG_PATH=/mnt/local_disk/enroot/config/${UID}/${SLURM_JOB_ID}
-export ENROOT_CACHE_PATH=/mnt/local_disk/enroot/cache/${UID}/${SLURM_JOB_ID}
-export ENROOT_DATA_PATH=/mnt/local_disk/enroot/data/${UID}/${SLURM_JOB_ID}
-
-mkdir -p ${ENROOT_RUNTIME_PATH}
-mkdir -p ${ENROOT_CONFIG_PATH}
-mkdir -p ${ENROOT_CACHE_PATH}
-mkdir -p ${ENROOT_DATA_PATH}
-
 
 CONFIG_FILE=${CONFIG_FILE:-"torchtitan/models/llama3/train_configs/debug_model.toml"}
 
@@ -27,4 +17,4 @@ srun --container-image=/home/ubuntu/torchtitan_cuda128_torch27.sqsh \
     --container-mounts=/home/ubuntu/.cache/huggingface:/root/.cache/huggingface \
    --container-writable \
    --no-container-mount-home \
-    bash run_train.sh --job.config_file ${CONFIG_FILE}
+   bash run_train.sh --job.config_file ${CONFIG_FILE}
