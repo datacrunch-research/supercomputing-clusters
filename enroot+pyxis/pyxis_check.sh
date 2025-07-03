c19b36e864778f3244afdc37fdaf649d49952ca3
roG0d <rodgarcas98@gmail.com>
2025-07-03 13:42:55 +0000

    DELETED: enroot+pyxis folder

--- enroot+pyxis/pyxis_check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=pyxis_test
-#SBATCH --output=pyxis_test.out
-#SBATCH --container-name=pyxis_test
-#SBATCH --container-image=docker://ubuntu
-
-echo "Running inside container:"
-grep PRETTY /etc/os-release
